5e49bdc7f9320a9c98311c30d61b418e648426ac
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-11-02 15:02:38 -0400

    built games migration

--- db/migrate/20191102175031_create_games.rb
+++ db/migrate/20191102175031_create_games.rb
@@ -1,4 +1,10 @@
 class CreateGame < ActiveRecord::Migration
   def change
+    create_table :games do |t|
+      t.string :name
+      t.string :genre
+      t.string :publisher
+      t.string :developer
+    end
   end
 end

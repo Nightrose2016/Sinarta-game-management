227210fc089aa3a5bc16db87e1843c7319335770
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-11-02 16:09:12 -0400

    built get login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,3 +18,10 @@ class UsersController < ApplicationController
         end
     end
     
+    get '/login' do
+        if !logged_in?
+            erb :'users/login'
+        else
+            redirect to('/games')
+        end
+      end

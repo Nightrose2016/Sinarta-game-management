2587d18569d92f447fea4e8e53ee3c26f4011146
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-11-02 18:20:19 -0400

    re did users controller and fixed spacing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+    user = User.new(params)
+    if user.save
+      session[:user_id] = user.id
+      redirect to('/games')
+    else
+      redirect to('/signup')
+    end
+  end
+    
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to('/games')
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to('/tweets')
+    else
+      redirect to('/login')
+      end
+    end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end  

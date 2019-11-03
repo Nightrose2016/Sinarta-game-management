e30474f6d47ed138e21281baa9f13635a8be1c5b
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-11-03 15:24:39 -0500

    added user slug

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,4 +1,9 @@
 class Users < ActiveRecord::Base
     has_many :games
     has_secure_password
+    validates :username, presence: true
+    
+    def slug
+        self.username.downcase.gsub(" ", "-")
+    end
 end

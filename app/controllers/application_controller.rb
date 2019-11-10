63f03a753b6f069bc6d2c4ef6110d6d4b16c54cd
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-11-10 15:22:12 -0500

    moved users helper to users frrom application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,20 +13,5 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  helpers do
-
-    def current_user
-      Users.find_by(id: session[:user_id])
-    end
-
-    def logged_in?
-      !!current_user
-    end
-
-    def redirect_if_not_logged_in
-      if !logged_in?
-        redirect to('/login')
-      end
-    end
-  end
+ 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,4 +48,21 @@ class UsersController < ApplicationController
     @user = User.find_by_slug(params[:slug])
     erb :'users/show'
   end
+
+  helpers do
+
+    def current_user
+      Users.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect to('/login')
+      end
+    end
+  end
 end

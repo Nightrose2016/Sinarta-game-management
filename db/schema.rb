cb5be6e5c238f9122a5cc416578b3d0971ae11a3
Nightrose2016 <40536248+Nightrose2016@users.noreply.github.com>
2019-11-11 13:51:49 -0500

    built userid-games migration and ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191102175031) do
+ActiveRecord::Schema.define(version: 20191111185023) do
 
   create_table "games", force: :cascade do |t|
-    t.string "name"
-    t.string "genre"
-    t.string "publisher"
-    t.string "developer"
+    t.string  "name"
+    t.string  "genre"
+    t.string  "publisher"
+    t.string  "developer"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
